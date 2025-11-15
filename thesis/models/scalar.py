cbf23973fb5543b4ce397ac8623607543af46c3e
Renat Sibgatulin <renat.sibgatulin@gmail.com>
2025-11-15 12:22:41 +0100

    Add models with marginalised σ_obs
    All works and works well

--- thesis/models/scalar.py
+++ thesis/models/scalar.py
@@ -17,6 +17,7 @@ def model_unpooled(
     prior={},
     obs: Float[Array, " *spatial"] | None = None,
 ):
+    """Simplest QSM model."""
     obs_scale = numpyro.sample(
         "obs_scale",
         dist.InverseGamma(
@@ -32,6 +33,30 @@ def model_unpooled(
         )
         with handlers.mask(mask=mask_fg):
             numpyro.sample("obs", dist.Normal(obs_loc, obs_scale), obs=obs)
+
+
+def model_unpooled_marginalised(
+    grid_shape: tuple[int, ...],
+    kernel_rft: Float[Array, " *rft"],
+    mask_fg: Bool[Array, " *spatial"] = jnp.array(True),
+    prior={},
+    obs: Float[Array, " *spatial"] | None = None,
+):
+    """Simplest QSM model with the noise STD marginalised out."""
+
+    prior_obs_scale = prior.get("obs_scale", {"concentration": 3, "rate": 5e-3})
+    obs_df = 2 * prior_obs_scale["concentration"]
+    obs_scale = prior_obs_scale["rate"] / prior_obs_scale["concentration"]
+
+    with numpyro.plate_stack("spatial", grid_shape):
+        x = numpyro.sample(
+            "chi", dist.Normal(**prior.get("chi", {"loc": 0, "scale": 1e-2}))
+        )
+        obs_loc = numpyro.deterministic(
+            "field", simulate_field_from_scalar_and_kernel(kernel_rft, x)
+        )
+        with handlers.mask(mask=mask_fg):
+            numpyro.sample("obs", dist.StudentT(obs_df, obs_loc, obs_scale), obs=obs)
 
 
 def model_pooled_decentred(
@@ -71,6 +96,40 @@ def model_pooled_decentred(
             numpyro.sample("obs", dist.Normal(obs_loc, obs_scale), obs=obs)
 
 
+def model_pooled_decentred_marginalised(
+    grid_shape: tuple[int, ...],
+    kernel_rft: Float[Array, " *rft"],
+    roi: Int[Array, " *spatial"],
+    nroi: int,
+    mask_fg: Bool[Array, " *spatial"] = jnp.array(True),
+    prior={},
+    obs: Float[Array, " *spatial"] | None = None,
+):
+    prior_obs_scale = prior.get("obs_scale", {"concentration": 3, "rate": 5e-3})
+    obs_df = 2 * prior_obs_scale["concentration"]
+    obs_scale = prior_obs_scale["rate"] / prior_obs_scale["concentration"]
+
+    with numpyro.plate("roi", nroi):
+        loc = numpyro.sample(
+            "chi_loc", dist.Normal(**prior.get("chi_loc", {"loc": 0, "scale": 5e-2}))
+        )
+        scale = numpyro.sample(
+            "chi_scale",
+            dist.TruncatedNormal(
+                **prior.get("chi_scale", {"loc": 2e-2, "scale": 5e-2}), low=0.0
+            ),
+        )
+
+    with numpyro.plate_stack("spatial", grid_shape):
+        x_eps = numpyro.sample("chi_eps", dist.Normal(0, 1))
+        x = numpyro.deterministic("chi", loc[roi] + scale[roi] * x_eps)
+        obs_loc = numpyro.deterministic(
+            "field", simulate_field_from_scalar_and_kernel(kernel_rft, x)
+        )
+        with handlers.mask(mask=mask_fg):
+            numpyro.sample("obs", dist.StudentT(obs_df, obs_loc, obs_scale), obs=obs)
+
+
 def model_pooled_centred(
     grid_shape: tuple[int, ...],
     kernel_rft: Float[Array, " *rft"],
@@ -105,3 +164,36 @@ def model_pooled_centred(
         )
         with handlers.mask(mask=mask_fg):
             numpyro.sample("obs", dist.Normal(obs_loc, obs_scale), obs=obs)
+
+
+def model_pooled_centred_marginalised(
+    grid_shape: tuple[int, ...],
+    kernel_rft: Float[Array, " *rft"],
+    roi: Int[Array, " *spatial"],
+    nroi: int,
+    mask_fg: Bool[Array, " *spatial"] = jnp.array(True),
+    prior={},
+    obs: Float[Array, " *spatial"] | None = None,
+):
+    prior_obs_scale = prior.get("obs_scale", {"concentration": 3, "rate": 5e-3})
+    obs_df = 2 * prior_obs_scale["concentration"]
+    obs_scale = prior_obs_scale["rate"] / prior_obs_scale["concentration"]
+
+    with numpyro.plate("roi", nroi):
+        loc = numpyro.sample(
+            "chi_loc", dist.Normal(**prior.get("chi_loc", {"loc": 0, "scale": 5e-2}))
+        )
+        scale = numpyro.sample(
+            "chi_scale",
+            dist.TruncatedNormal(
+                **prior.get("chi_scale", {"loc": 2e-2, "scale": 5e-2}), low=0.0
+            ),
+        )
+
+    with numpyro.plate_stack("spatial", grid_shape):
+        x = numpyro.sample("chi", dist.Normal(loc[roi], scale[roi]))
+        obs_loc = numpyro.deterministic(
+            "field", simulate_field_from_scalar_and_kernel(kernel_rft, x)
+        )
+        with handlers.mask(mask=mask_fg):
+            numpyro.sample("obs", dist.StudentT(obs_df, obs_loc, obs_scale), obs=obs)
