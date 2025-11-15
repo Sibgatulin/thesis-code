5b410244f55f90e6d0c68f9d8240c6fc47e039ca
Renat Sibgatulin <renat.sibgatulin@gmail.com>
2025-11-15 17:20:43 +0100

    Add working unpooled model in kspace

--- thesis/models/scalar.py
+++ thesis/models/scalar.py
@@ -238,3 +238,46 @@ def model_pooled_centred_marginalised(
             numpyro.sample(
                 "obs", dist.StudentT(obs_df, obs_loc, jnp.sqrt(obs_var)), obs=obs
             )
+
+
+def model_rft_log_unpooled(
+    grid_shape: tuple[int, ...],
+    kernel_rft: Float[Array, " *rft"],
+    mask_fg: Bool[Array, " *spatial"] = jnp.array(True),
+    prior={},
+    obs: Float[Array, " *spatial"] | None = None,
+    ft_norm="backward",
+):
+    ndim = len(grid_shape)
+    ft_axes = tuple(range(-ndim, 0))
+    rft_shape = kernel_rft.shape
+
+    @jit
+    def _compute_all(x_ft_log_abs, x_ft_arg):
+        x_ft = jnp.exp(x_ft_log_abs + 1j * x_ft_arg).at[(0,) * ndim].set(0.0)
+        x = jnp.fft.irfftn(x_ft, axes=ft_axes, norm=ft_norm)
+        field = jnp.fft.irfftn(kernel_rft * x_ft, axes=ft_axes, norm=ft_norm)
+        return x_ft, x, field
+
+    obs_var = numpyro.sample(
+        "obs_var",
+        dist.InverseGamma(
+            **prior.get("obs_var", {"concentration": 3, "rate": 5e-3**2}),
+        ),
+    )
+
+    with numpyro.plate_stack("rft", rft_shape):
+        # Define the rest of the prior in the Fourier space
+        _prior_ft_abs = prior.get("_ft_log_abs", {"loc": 0.0, "scale": 1.0})
+        x_ft_log_abs = numpyro.sample(
+            "_chi_ft_log_abs",
+            dist.Normal(_prior_ft_abs["loc"], _prior_ft_abs["scale"]),
+        )
+        x_ft_arg = numpyro.sample("_ft_arg", dist.Uniform(low=-jnp.pi, high=jnp.pi))
+        x_ft, x, field = _compute_all(x_ft_log_abs, x_ft_arg)
+        numpyro.deterministic("chi_ft", x_ft)
+    with numpyro.plate_stack("spatial", grid_shape):
+        numpyro.deterministic("chi", x)
+        obs_loc = numpyro.deterministic("field", field)
+        with handlers.mask(mask=mask_fg):
+            numpyro.sample("obs", dist.Normal(obs_loc, jnp.sqrt(obs_var)), obs=obs)
