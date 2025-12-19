227f4040d9c07fadd79f11ee353c4dab8759ed8e
Renat Sibgatulin <renat.sibgatulin@gmail.com>
2025-12-19 14:16:43 +0100

    Marginalise obs_var

--- thesis/models/csst.py
+++ thesis/models/csst.py
@@ -15,19 +15,19 @@ DEFAULT_PRIOR = {
 }
 
 
-def model_unpooled(
+def model_unpooled_marginalised_unconstrained_msa(
     b_dir: Float[Array, "orient ndim"],
     v1: Float[Array, "*spatial ndim"],
     mask_fg: Bool[Array, " *spatial"] = jnp.array(True),
     prior={},
     obs: Float[Array, " *spatial"] | None = None,
 ):
-    """Simplest CSST model.
+    """Simplest CSST model with the noise STD marginalised out.
 
     mms(r) ~ N(0, σ_mms)
-    msa(r) ~ N(0, σ_msa)
+    msa(r) ~ N(0, σ_msa) # WRONG AND TEMPORARY
     σ²_obs ~ Γ^(-1)(α, β)
-    obs ~ N(f(mms, msa), σ_obs)
+    obs ~ StudentT(2α, f(mms, msa), √β/α)
     """
     b_dir = jnp.atleast_2d(b_dir)
     norient, ndim = b_dir.shape
@@ -36,10 +36,10 @@ def model_unpooled(
         f"Inconsistent dimensionality {v1.shape=} & {b_dir.shape=}"
     )
 
-    obs_var = numpyro.sample(
-        "obs_var",
-        dist.InverseGamma(**prior.get("obs_var", DEFAULT_PRIOR["obs_var"])),
-    )
+    prior_obs_var = prior.get("obs_var", DEFAULT_PRIOR["obs_var"])
+    obs_df = 2 * prior_obs_var["concentration"]
+    obs_var = prior_obs_var["rate"] / prior_obs_var["concentration"]
+
     with numpyro.plate_stack("spatial", grid_shape):
         mms = numpyro.sample(
             "mms", dist.Normal(**prior.get("mms", DEFAULT_PRIOR["mms"]))
@@ -53,4 +53,6 @@ def model_unpooled(
         )
         with numpyro.plate("orient", norient):
             with handlers.mask(mask=mask_fg):
-                numpyro.sample("obs", dist.Normal(obs_loc, jnp.sqrt(obs_var)), obs=obs)
+                numpyro.sample(
+                    "obs", dist.StudentT(obs_df, obs_loc, jnp.sqrt(obs_var)), obs=obs
+                )
